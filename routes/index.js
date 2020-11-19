ac7b342fc4c9ce3f02562dbb51abcc6786f57095
TreeLognan <Akananyotkeawlamoon2@gmail.com>
2020-11-19 10:21:17 +0700

    [EDITED] for reveice the lead email by form

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ var router = express.Router();
 let landing = require("../controllers/landing");
 /* GET home page. */
 router.get("/", landing.get_landing);
-
+router.post("/", landing.lead_email);
 module.exports = router;
